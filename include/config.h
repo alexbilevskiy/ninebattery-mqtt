ab892cad61c24c0fda119bc356e08258e368d7ca
Alex <thealexsb@gmail.com>
2022-10-23 11:07:24 +0300

    fix typo

--- include/config.h
+++ include/config.h
@@ -1,6 +1,3 @@
-#ifndef ESP8266_AIR_CONFIG_H
-#define ESP8266_AIR_CONFIG_H
-
 #define wifi_ssid "ssid"
 #define wifi_password "password"
 #define mqtt_server "192.168.1.1"
@@ -12,5 +9,3 @@
 #define mqttTopic "wifi2mqtt/%s"
 #define mqttClientId "ninebattery-mqtt"
 #define mqttEnabled true
-
-#endif //ESP8266_AIR_CONFIG_H
